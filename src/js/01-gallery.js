9a7f97cb3a256512e8d2f5ae59fd5a61c9f0eaa3
Demon1056 <102851685+Demon1056@users.noreply.github.com>
2022-08-28 11:47:50 +0300

    remove require

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,6 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
- require ('SimpleLightbox');
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 const galleryHtml= galleryItems.map(creatOneImage).join('')
